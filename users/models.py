c0e7376bf7e69dcc919df19fa7c87978c6f81a73
xxgray <xxgray@users.noreply.github.com>
2019-11-28 20:24:04 +0900

    #18.0
    Getting Authorization Code

--- users/models.py
+++ users/models.py
@@ -33,12 +33,12 @@ class User(AbstractUser):
 
     LOGIN_EMAIL = "email"
     LOGIN_GITHUB = "github"
-    LOGING_KAKAO = "kakao"
+    LOGIN_KAKAO = "kakao"
 
     LOGIN_CHOICES = (
         (LOGIN_EMAIL, "Email"),
         (LOGIN_GITHUB, "Github"),
-        (LOGING_KAKAO, "Kakao"),
+        (LOGIN_KAKAO, "Kakao"),
     )
 
     avatar = models.ImageField(upload_to="avatars", blank=True)
@@ -73,4 +73,5 @@ class User(AbstractUser):
                 fail_silently=False,
                 html_message=html_message,
             )
+            self.save()
         return
