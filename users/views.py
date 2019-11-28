2ca32cd2d4ab12970ca67bf225fabb430abcae28
xxgray <xxgray@users.noreply.github.com>
2019-11-29 01:09:26 +0900

    #18.2
    Getting Kakao Profile Photo

--- users/views.py
+++ users/views.py
@@ -4,6 +4,7 @@ from django.views.generic import FormView
 from django.urls import reverse_lazy
 from django.shortcuts import redirect, reverse
 from django.contrib.auth import authenticate, login, logout
+from django.core.files.base import ContentFile
 from . import forms, models
 
 
@@ -116,7 +117,6 @@ def github_callback(request):
                         )
                         user.set_unusable_password()
                         user.save()
-
                     login(request, user)
                     return redirect(reverse("core:home"))
                 else:
@@ -179,6 +179,12 @@ def kakao_callback(request):
             )
             user.set_unusable_password()
             user.save()
+            if profile_image is not None:
+                Photo_request = requests.get(profile_image)
+
+                user.avatar.save(
+                    f"{nickname}-avatar", ContentFile(Photo_request.content)
+                )
         login(request, user)
         return redirect(reverse("core:home"))
     except KakaoException:
