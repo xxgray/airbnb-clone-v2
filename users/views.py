a34f224b495e6fff160a7db10db9355e39ac080e
xxgray <xxgray@users.noreply.github.com>
2019-11-30 03:19:14 +0900

    #22.0
    Photo Grid

--- users/views.py
+++ users/views.py
@@ -239,8 +239,8 @@ class UpdateProfileView(mixins.LoggedInOnlyView, SuccessMessageMixin, UpdateView
 
 
 class UpdatePasswordView(
-    mixins.EmailLoginOnlyView,
     mixins.LoggedInOnlyView,
+    mixins.EmailLoginOnlyView,
     SuccessMessageMixin,
     PasswordChangeView,
 ):
